89e0ce37b56ca6d90fd93562c52036c577241f96
salzubi <salaheddin@sentient.xyz>
2024-12-05 17:03:19 +0000

    Fix

--- src/sentientsearch/build_context.py
+++ src/sentientsearch/build_context.py
@@ -65,11 +65,7 @@ def build_context(sources_result, query, pro_mode, date_context):
                     combined_list.append(answer_box[key])
 
         if pro_mode:
-            start_time = time.time()
             final_list = get_reranking_nvidia(combined_list, query + date_context, 15)
-
-            reranking_time = time.time() - start_time
-            logger.info(f"Reranking took {reranking_time:.2f} seconds")
         else:
             final_list = combined_list
 
